99d5f1edafb86e7d03d5215f04f7f0d0e7600676
danilamaster1 <howarddan11@mail.ru>
2022-12-11 18:58:48 +0300

    add_logs

--- log/server_log_config.py
+++ log/server_log_config.py
@@ -1,1 +1,10 @@
 import logging
+
+log = logging.getLogger('server')
+
+logging.basicConfig(filename='server_log.log',
+                    encoding='utf-8',
+                    format="%(asctime)s %(levelname)s %(module)s %(message)s",
+                    level=logging.INFO)
+
+

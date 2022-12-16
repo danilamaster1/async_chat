b2bd7c84c313914289ac5965476366c403ada8e0
Daniel <98400545+danilamaster1@users.noreply.github.com>
2022-12-16 16:21:47 +0300

    Decorators (#2)
    * add_log_decorator

--- client.py
+++ client.py
@@ -7,16 +7,29 @@ import sys
 import json
 import time
 import logging
+import inspect
 sys.path.append('common')
 sys.path.append('log')
 from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
     RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
 from common.utils import get_message, send_message
 import log.client_log_config
+from functools import wraps
 
 logs = logging.getLogger('client')
 
 
+def decor_log(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        outer_func = inspect.stack()[1][3]
+        logs.info(f'Функция {func.__name__} вызвана из функции {outer_func}')
+        r = func(*args, **kwargs)
+        return r
+    return wrapper
+
+
+@decor_log
 def create_presence(account_name='Guest'):
     out = {
         ACTION: PRESENCE,
@@ -28,6 +41,7 @@ def create_presence(account_name='Guest'):
     return out
 
 
+@decor_log
 def process_ans(message):
     if RESPONSE in message:
         if message[RESPONSE] == 200:
@@ -50,7 +64,13 @@ def main():
         sys.exit(1)
 
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.connect((server_address, server_port))
+
+    try:
+        transport.connect((server_address, server_port))
+    except ConnectionRefusedError:
+        logs.error('Сервер не найден.')
+        sys.exit(1)
+
     message_to_server = create_presence()
     send_message(transport, message_to_server)
     try:

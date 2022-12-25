46c2e1ec122a663ebcdeea54b916f7f0bfa54945
danilamaster1 <howarddan11@mail.ru>
2022-12-25 22:14:34 +0300

    select

--- common/utils.py
+++ common/utils.py
@@ -1,7 +1,12 @@
 import json
+import sys
 from variables import MAX_PACKAGE_LENGTH, ENCODING
+from errors import IncorrectDataRecivedError, NonDictInputError
+from decos import log
+sys.path.append('../')
 
 
+@log
 def get_message(client):
     encoded_response = client.recv(MAX_PACKAGE_LENGTH)
     if isinstance(encoded_response, bytes):
@@ -9,11 +14,14 @@ def get_message(client):
         response = json.loads(json_response)
         if isinstance(response, dict):
             return response
-        raise ValueError
-    raise ValueError
+        raise IncorrectDataRecivedError
+    raise IncorrectDataRecivedError
 
 
+@log
 def send_message(sock, message):
+    if not isinstance(message, dict):
+        raise NonDictInputError
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)

20506f6118dc66429856c23793bc47bfaea60f5d
Daniel <98400545+danilamaster1@users.noreply.github.com>
2022-12-25 22:15:05 +0300

    select (#3)

---
+++ decos.py
@@ -0,0 +1,18 @@
+import sys
+import logging
+import logs.server_log_config
+import logs.client_log_config
+
+if sys.argv[0].find('client') == -1:
+    LOGGER = logging.getLogger('server')
+else:
+    LOGGER = logging.getLogger('client')
+
+
+def log(func_to_log):
+    def log_saver(*args, **kwargs):
+        ret = func_to_log(*args, **kwargs)
+        LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}. '
+                     f'Вызов из модуля {func_to_log.__module__}')
+        return ret
+    return log_saver

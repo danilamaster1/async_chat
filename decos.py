8b7b50aeb08975c505e4b0c5e2b49a6d3c595483
danilamaster1 <howarddan11@mail.ru>
2023-01-25 18:52:40 +0300

    db

--- decos.py
+++ decos.py
@@ -1,18 +1,21 @@
 import sys
-import logging
+sys.path.append('logs')
 import logs.server_log_config
 import logs.client_log_config
+import logging
 
+# метод определения модуля, источника запуска.
 if sys.argv[0].find('client') == -1:
-    LOGGER = logging.getLogger('server')
+    #если не клиент то сервер!
+    logger = logging.getLogger('server')
 else:
-    LOGGER = logging.getLogger('client')
+    # ну, раз не сервер, то клиент
+    logger = logging.getLogger('client')
 
 
 def log(func_to_log):
-    def log_saver(*args, **kwargs):
-        ret = func_to_log(*args, **kwargs)
-        LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}. '
-                     f'Вызов из модуля {func_to_log.__module__}')
+    def log_saver(*args , **kwargs):
+        logger.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args} , {kwargs}. Вызов из модуля {func_to_log.__module__}')
+        ret = func_to_log(*args , **kwargs)
         return ret
     return log_saver
--- launcher.py
+++ launcher.py
@@ -9,10 +9,10 @@ while True:
     elif action == 's':
         clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
         # Запускаем сервер!
-        process.append(subprocess.Popen('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
+        process.append(subprocess.Popen('python server.py', shell=True))
         # Запускаем клиентов:
         for i in range(clients_count):
-            process.append(subprocess.Popen(f'python client.py -n test{i + 1}', creationflags=subprocess.CREATE_NEW_CONSOLE))
+            process.append(subprocess.Popen(f'python client.py -n test{i + 1}', shell=True))
     elif action == 'x':
         while process:
             process.pop().kill()
